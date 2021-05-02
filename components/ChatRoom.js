5c177a9617aa7dee1e160e80dde835eb76714921
smarqueslopez <smarqueslopez@gmail.com>
2021-05-03 00:03:35 +0200

    Addin chat room component

--- components/ChatRoom.js
+++ components/ChatRoom.js
@@ -1,24 +1,86 @@
+import { Avatar, IconButton, TextField } from '@material-ui/core'
 import styled from 'styled-components'
+import RoomMenu from './RoomMenu'
 import ComputerIcon from '@material-ui/icons/Computer'
+import { useParams } from 'react-router'
+import { useEffect, useState } from 'react'
+import { db } from '../firebase'
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon'
+import AttachFileIcon from '@material-ui/icons/AttachFile'
+import MicIcon from '@material-ui/icons/Mic'
 
 function ChatRoom() {
+  const { id } = useParams()
+
+  const [title, setTitle] = useState('')
+  const [avatar, setAvatar] = useState('')
+
+  useEffect(() => {
+    if (id) {
+      db.collection('rooms')
+        .doc(id)
+        .onSnapshot((snapshot) => {
+          setTitle(snapshot.data().title)
+          setAvatar(snapshot.data().avatar)
+        })
+    }
+  }, [id])
+
   return (
-    <EmptyContainer>
-      <Image />
-      <Intro>Keep your phone connected</Intro>
-      <IntroSecondary>
-        WhatsApp Web connects to your phone to sync messages. To reduce data
-        usage, connect your phone to Wi-Fi.
-      </IntroSecondary>
-      <GetAppMsg>
-        <ComputerIcon />
-        <span>WhatsApp is available for Windows.</span>
-        <a href='' target='_blank'>
-          Get it here
-        </a>
-        .
-      </GetAppMsg>
-    </EmptyContainer>
+    <>
+      {id ? (
+        <Container>
+          <HeaderContainer>
+            <TitleContainer>
+              <AvatarContainer src={avatar} />
+              <InfoRoom>
+                <TitleRoom>{title}</TitleRoom>
+                <UserRoom>Users</UserRoom>
+              </InfoRoom>
+            </TitleContainer>
+            <IconsContainer>
+              <RoomMenu />
+            </IconsContainer>
+          </HeaderContainer>
+          <ChatContainer></ChatContainer>
+          <InputContainer>
+            <IconsContainer>
+              <IconButton>
+                <InsertEmoticonIcon />
+              </IconButton>
+              <IconButton>
+                <AttachFileIcon />
+              </IconButton>
+            </IconsContainer>
+            <MessageInputContainer>
+              <MessageInput placeholder='Write a message here' />
+            </MessageInputContainer>
+            <IconsContainer>
+              <IconButton>
+                <MicIcon />
+              </IconButton>
+            </IconsContainer>
+          </InputContainer>
+        </Container>
+      ) : (
+        <EmptyContainer>
+          <Image />
+          <Intro>Keep your phone connected</Intro>
+          <IntroSecondary>
+            WhatsApp Web connects to your phone to sync messages. To reduce data
+            usage, connect your phone to Wi-Fi.
+          </IntroSecondary>
+          <GetAppMsg>
+            <ComputerIcon />
+            <span>WhatsApp is available for Windows.</span>
+            <a href='' target='_blank'>
+              Get it here
+            </a>
+            .
+          </GetAppMsg>
+        </EmptyContainer>
+      )}
+    </>
   )
 }
 
@@ -52,12 +114,14 @@ const Intro = styled.div`
   font-size: 36px;
   font-weight: 300;
   color: #525252;
+  text-align: center;
 `
 const IntroSecondary = styled.div`
   margin-top: 24px;
   font-size: 14px;
   line-height: 20px;
   color: rgba(0, 0, 0, 0.45);
+  text-align: center;
 `
 const GetAppMsg = styled.div`
   display: flex;
@@ -74,6 +138,120 @@ const GetAppMsg = styled.div`
 
   a {
     color: #07bc4c;
+  }
+`
+
+const HeaderContainer = styled.section`
+  height: 80px;
+  width: 100%;
+  padding: 15px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  z-index: 1;
+  background-color: #ededed;
+  border-bottom: 1px solid whitesmoke;
+`
+
+const TitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const InfoRoom = styled.div`
+  padding: 5px 15px;
+  display: flex;
+  flex-direction: column;
+`
+
+const AvatarContainer = styled(Avatar)``
+
+const TitleRoom = styled.div``
+
+const UserRoom = styled.div``
+
+const IconsContainer = styled.div`
+  &&& {
+    .MuiSvgIcon-root {
+      color: rgb(145, 145, 145);
+    }
+  }
+`
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`
+
+const ChatContainer = styled.div`
+  width: 100%;
+  height: calc(100% - 80px - 80px);
+  max-height: calc(100% - 80px - 80px);
+  background-image: url('/images/background-chat.png');
+  overflow-y: auto;
+
+  ::-webkit-scrollbar {
+    width: 6px;
+    height: 6px;
+    background-color: #f5f5f5;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: rgba(0, 0, 0, 0.2);
+  }
+
+  ::-webkit-scrollbar-track {
+    background-color: rgba(255, 255, 255, 0.08);
+  }
+`
+
+const InputContainer = styled.div`
+  width: 100%;
+  height: 80px;
+  padding: 0 5px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #ededed;
+  border-top: 1px solid whitesmoke;
+`
+
+const MessageInputContainer = styled.div`
+  width: calc(100% - 96px - 48px);
+  padding: 0 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const MessageInput = styled(TextField)`
+  outline-width: 0;
+  border: none;
+  flex: 1;
+
+  &&& {
+    padding: 0 15px;
+    border-radius: 25px;
+    background-color: white;
+
+    .MuiInputBase-root {
+      border-bottom: none;
+
+      &::before {
+        border-bottom: none;
+      }
+
+      &.MuiInput-underline:after {
+        border-bottom: none;
+      }
+
+      .MuiInputAdornment-root {
+        color: rgb(145, 145, 145);
+      }
+    }
   }
 `
 
---
+++ components/RoomMenu.js
@@ -0,0 +1,48 @@
+import { IconButton, Menu, MenuItem } from '@material-ui/core'
+import { useState } from 'react'
+import SearchIcon from '@material-ui/icons/Search'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+
+function RoomMenu({ type }) {
+  const [anchorMenu, setAnchorMenu] = useState(null)
+  const openMenu = (event) => setAnchorMenu(event.currentTarget)
+  const closeMenu = () => setAnchorMenu(null)
+
+  return (
+    <>
+      <IconButton>
+        <SearchIcon />
+      </IconButton>
+      <IconButton onClick={openMenu}>
+        <MoreVertIcon />
+      </IconButton>
+      <Menu
+        anchorEl={anchorMenu}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'left'
+        }}
+        keepMounted
+        getContentAnchorEl={null}
+        open={Boolean(anchorMenu)}
+        onClose={closeMenu}
+      >
+        {type === 'user' ? (
+          <MenuItem onClick={closeMenu}>Contact Info</MenuItem>
+        ) : (
+          <MenuItem onClick={closeMenu}>Group Info</MenuItem>
+        )}
+        <MenuItem onClick={closeMenu}>Select Messages</MenuItem>
+        <MenuItem onClick={closeMenu}>Silent Notifications</MenuItem>
+        <MenuItem onClick={closeMenu}>Empty Messages</MenuItem>
+        {type === 'user' ? (
+          <MenuItem onClick={closeMenu}>Remove Chat</MenuItem>
+        ) : (
+          <MenuItem onClick={closeMenu}>Remove Group</MenuItem>
+        )}
+      </Menu>
+    </>
+  )
+}
+
+export default RoomMenu
