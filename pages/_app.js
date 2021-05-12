0c2892f0898ba6db50bf9ccc09c4aaa134a8f481
smarqueslopez <smarqueslopez@gmail.com>
2021-05-12 17:18:12 +0200

    Remove loading component

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,11 @@ import '../styles/globals.css'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { auth, db } from '../firebase'
 import Login from './login'
-import Loading from '../components/Loading'
 import { useEffect } from 'react'
 import firebase from 'firebase'
 
 function MyApp({ Component, pageProps }) {
-  const [user, loading] = useAuthState(auth)
+  const [user] = useAuthState(auth)
 
   useEffect(() => {
     if (user) {
@@ -21,8 +20,6 @@ function MyApp({ Component, pageProps }) {
       )
     }
   }, [user])
-
-  if (loading) return <Loading />
 
   if (!user) return <Login />
 
